acdf579a50272b96e20959c3d05dd58ad178f544
kotiswamy <swamy61802@gmail.com>
2023-08-13 15:42:44 +0530

    Changes done

--- Task 4 - Jquery/Kotiswamy/script.js
+++ Task 4 - Jquery/Kotiswamy/script.js
@@ -155,11 +155,11 @@ $(document).ready(function () {
   let headOfAccountBoolean = false;
 
   let headOfAccountList = {
-    "0853001020002000000NVN": { balance: 1000000, loc: 5000 },
-    "8342001170004001000NVN": { balance: 1008340, loc: 4000 },
-    "2071011170004320000NVN": { balance: 14530000, loc: 78000 },
-    "8342001170004002000NVN": { balance: 1056400, loc: 34000 },
-    "2204000030006300303NVN": { balance: 123465400, loc: 5000 },
+    0: { balance: 1000000, loc: 5000 },
+    1: { balance: 1008340, loc: 4000 },
+    2: { balance: 14530000, loc: 78000 },
+    3: { balance: 1056400, loc: 34000 },
+    4: { balance: 123465400, loc: 5000 },
   };
 
   $(".headOfAccountEL").change(function () {
@@ -190,15 +190,15 @@ $(document).ready(function () {
   let expenditureTypeBoolean = false;
 
   let expenditureTypeList = {
-    "Capital Expenditure": [
+    0: [
       "Maintain current levels of operation within the organization",
       "Expenses to permit future expansion.",
     ],
-    "Revenue Expenditure": [
+    1: [
       "Sales costs or All expenses incurred by the firm that are directly tied to the manufacture and selling of its goods or services.",
       "All expenses incurred by the firm to guarantee the smooth operation.",
     ],
-    "Deferred Revenue Expenditure": [
+    2: [
       "Exorbitant Advertising Expenditures",
       "Unprecedented Losses",
       "Development and Research Cost",
@@ -207,7 +207,7 @@ $(document).ready(function () {
 
   $(".expenditureTypeEl").change(function () {
     $(".purposeTypeEl").empty();
-    $(".purposeTypeEl").append(`<option selected></option>`);
+    $(".purposeTypeEl").append(`<option value="">Select</option>`);
     for (let i in expenditureTypeList) {
       if (i === $(this).val()) {
         for (let j of expenditureTypeList[i]) {
@@ -254,6 +254,117 @@ $(document).ready(function () {
     }
   });
 
+  // partyAmountEl
+
+  let below20List = [
+    "",
+    "one ",
+    "two ",
+    "three ",
+    "four ",
+    "five ",
+    "six ",
+    "seven ",
+    "eight ",
+    "nine ",
+    "ten ",
+    "eleven ",
+    "twelve ",
+    "thirteen ",
+    "fourteen ",
+    "fifteen ",
+    "sixteen ",
+    "seventeen ",
+    "eighteen ",
+    "nineteen ",
+  ];
+  let above20List = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  function convertAmountToWords(amount) {
+    if ((amount = amount.toString()).length > 14) {
+      return "Amount out of range...";
+    }
+    var n = ("000000000000" + amount)
+      .substr(-14)
+      .match(/^(\d{2})(\d{2})(\d{1})(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+
+    var amountInWord = "";
+    amountInWord +=
+      n[1] != 0
+        ? (below20List[Number(n[1])] ||
+            above20List[n[1][0]] + " " + below20List[n[1][1]]) + "lakh "
+        : "";
+    amountInWord +=
+      n[2] != 0
+        ? (below20List[Number(n[2])] ||
+            above20List[n[2][0]] + " " + below20List[n[2][1]]) + "thousand "
+        : "";
+    amountInWord +=
+      n[3] != 0
+        ? (below20List[Number(n[3])] ||
+            above20List[n[3][0]] + " " + below20List[n[3][1]]) + "hundred and "
+        : "";
+    amountInWord +=
+      n[4] != 0
+        ? (below20List[Number(n[4])] ||
+            above20List[n[4][0]] + " " + below20List[n[4][1]]) + "crore "
+        : "";
+    amountInWord +=
+      n[5] != 0
+        ? (below20List[Number(n[5])] ||
+            above20List[n[5][0]] + " " + below20List[n[5][1]]) + "lakh "
+        : "";
+    amountInWord +=
+      n[6] != 0
+        ? (below20List[Number(n[6])] ||
+            above20List[n[6][0]] + " " + below20List[n[6][1]]) + "thousand "
+        : "";
+    amountInWord +=
+      n[7] != 0
+        ? (below20List[Number(n[7])] ||
+            above20List[n[7][0]] + " " + below20List[n[7][1]]) + "hundred "
+        : "";
+    amountInWord +=
+      n[8] != 0
+        ? (amountInWord != "" ? "and " : "") +
+          (below20List[Number(n[8])] ||
+            above20List[n[8][0]] + " " + below20List[n[8][1]]) +
+          "only "
+        : "";
+    return amountInWord;
+  }
+
+  let partyAmountBoolean = false;
+
+  $(".partyAmountErrMsg").hide();
+
+  $(".partyAmountEl").blur(function (event) {
+    if (event.target.value === "") {
+      $(".partyAmountErrMsg").show().text("Please enter party amount");
+      $(".partyAmountWordsEl").text("Amount in words...");
+      partyAmountBoolean = false;
+    } else if ($(this).val().match(/\D/)) {
+      $(".partyAmountErrMsg").show().text("Amount should be in number");
+      $(".partyAmountWordsEl").text("Amount in words...");
+      partyAmountBoolean = false;
+    } else {
+      partyAmountBoolean = true;
+      $(".partyAmountErrMsg").hide();
+      $(".partyAmountWordsEl").text(convertAmountToWords($(this).val()));
+    }
+  });
+
   // chooseFilesCard
 
   let addedFilesList = [];
@@ -268,7 +379,7 @@ $(document).ready(function () {
       fileChoosenBoolean = false;
     } else if (addedFilesList.includes($(".fileEl").val())) {
       fileChoosenBoolean = true;
-      $(".fileErrMsg").show().text("This file already uploaded");
+      $(".fileErrMsg").show().text("This file already Added");
     } else if (!addedFilesList.includes($(".fileEl").val())) {
       addedFilesList = [...addedFilesList, $(".fileEl").val()];
       fileChoosenBoolean = true;
@@ -287,118 +398,6 @@ $(document).ready(function () {
         );
         $(this).parent().remove();
       });
-    }
-  });
-
-  // partyAmountEl
-
-  let partyAmountBoolean = false;
-
-  $(".partyAmountErrMsg").hide();
-
-  $(".partyAmountEl").blur(function (event) {
-    if (event.target.value === "") {
-      $(".partyAmountErrMsg").show().text("Please enter party amount");
-      $(".partyAmountWordsEl").text("Amount in words...");
-      partyAmountBoolean = false;
-    } else if ($(this).val().match(/\D/)) {
-      $(".partyAmountErrMsg").show().text("Amount should be in number");
-      $(".partyAmountWordsEl").text("Amount in words...");
-      partyAmountBoolean = false;
-    } else {
-      partyAmountBoolean = true;
-      $(".partyAmountErrMsg").hide();
-      let below20List = [
-        "",
-        "one ",
-        "two ",
-        "three ",
-        "four ",
-        "five ",
-        "six ",
-        "seven ",
-        "eight ",
-        "nine ",
-        "ten ",
-        "eleven ",
-        "twelve ",
-        "thirteen ",
-        "fourteen ",
-        "fifteen ",
-        "sixteen ",
-        "seventeen ",
-        "eighteen ",
-        "nineteen ",
-      ];
-      let above20List = [
-        "",
-        "",
-        "twenty",
-        "thirty",
-        "forty",
-        "fifty",
-        "sixty",
-        "seventy",
-        "eighty",
-        "ninety",
-      ];
-
-      function convertAmountToWords(amount) {
-        if ((amount = amount.toString()).length > 14) {
-          return "Amount out of range...";
-        }
-        var n = ("000000000000" + amount)
-          .substr(-14)
-          .match(/^(\d{2})(\d{2})(\d{1})(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-
-        var amountInWord = "";
-        amountInWord +=
-          n[1] != 0
-            ? (below20List[Number(n[1])] ||
-                above20List[n[1][0]] + " " + below20List[n[1][1]]) + "lakh "
-            : "";
-        amountInWord +=
-          n[2] != 0
-            ? (below20List[Number(n[2])] ||
-                above20List[n[2][0]] + " " + below20List[n[2][1]]) + "thousand "
-            : "";
-        amountInWord +=
-          n[3] != 0
-            ? (below20List[Number(n[3])] ||
-                above20List[n[3][0]] + " " + below20List[n[3][1]]) +
-              "hundred and "
-            : "";
-        amountInWord +=
-          n[4] != 0
-            ? (below20List[Number(n[4])] ||
-                above20List[n[4][0]] + " " + below20List[n[4][1]]) + "crore "
-            : "";
-        amountInWord +=
-          n[5] != 0
-            ? (below20List[Number(n[5])] ||
-                above20List[n[5][0]] + " " + below20List[n[5][1]]) + "lakh "
-            : "";
-        amountInWord +=
-          n[6] != 0
-            ? (below20List[Number(n[6])] ||
-                above20List[n[6][0]] + " " + below20List[n[6][1]]) + "thousand "
-            : "";
-        amountInWord +=
-          n[7] != 0
-            ? (below20List[Number(n[7])] ||
-                above20List[n[7][0]] + " " + below20List[n[7][1]]) + "hundred "
-            : "";
-        amountInWord +=
-          n[8] != 0
-            ? (amountInWord != "" ? "and " : "") +
-              (below20List[Number(n[8])] ||
-                above20List[n[8][0]] + " " + below20List[n[8][1]]) +
-              "only "
-            : "";
-        return amountInWord;
-      }
-
-      $(".partyAmountWordsEl").text(convertAmountToWords($(this).val()));
     }
   });
 

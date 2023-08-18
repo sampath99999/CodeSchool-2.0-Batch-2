00168352c55146f1c0bec7fde8c644a620d82b7e
sainikhilallugure <sainikhilallugure@gmail.com>
2023-08-18 16:22:02 +0530

    adding task 5

---
+++ Task 5 - Sql/sainikhil/insert queries/insert queries_products.sql
@@ -0,0 +1,539 @@
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Picture Frames',
+        914.29,
+        900,
+        'Etiam faucibus cursus urna.',
+        7,
+        6
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Monopoly',
+        832.37,
+        64,
+        'Pellentesque at nulla.',
+        4,
+        2
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Stud Earrings',
+        112.28,
+        110,
+        'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique.',
+        6,
+        3
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'LEGO Sets',
+        753.96,
+        225,
+        'Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.',
+        6,
+        2
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'To Kill a Mockingbird',
+        622.1,
+        47,
+        'In hac habitasse platea dictumst.',
+        9,
+        5
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Broccoli',
+        102.27,
+        922,
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.',
+        8,
+        4
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        ' Foundation ',
+        997.1,
+        55,
+        'Nunc nisl.',
+        9,
+        1
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'nulla pede',
+        197.97,
+        491,
+        'Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci.',
+        10,
+        8
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Sticky Notes',
+        179.05,
+        250,
+        'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam. Nam tristique tortor eu pede.',
+        9,
+        5
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Classic Cufflinks',
+        425.08,
+        8,
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.',
+        1,
+        3
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Sleeping Bag',
+        864.95,
+        99,
+        'Cras non velit nec nisi vulputate nonummy.',
+        2,
+        10
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Rice',
+        920.17,
+        343,
+        'Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+        1,
+        4
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Decorative Tray',
+        280.24,
+        312,
+        'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy.',
+        4,
+        6
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Barbie Dolls',
+        167.14,
+        88,
+        'Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',
+        6,
+        2
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Tire Inflator',
+        516.29,
+        88,
+        'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
+        7,
+        8
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Smartwatches',
+        727.76,
+        432,
+        'Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.',
+        10,
+        3
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Wall Shelves',
+        394.97,
+        31,
+        'Donec posuere metus vitae ipsum. Aliquam non mauris.',
+        5,
+        6
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'The Power of Habit',
+        919.71,
+        391,
+        'Nulla ut erat id mauris vulputate elementum. Nullam varius.',
+        5,
+        5
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'RC Cars',
+        801.42,
+        62,
+        'Etiam vel augue.',
+        9,
+        2
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        ' Wireless Earbuds',
+        345.73,
+        18,
+        'Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+        8,
+        9
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'The Diary of a Young Girl',
+        624.91,
+        152,
+        'Aenean lectus.',
+        4,
+        5
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Scarf',
+        509.67,
+        692,
+        'Nulla tempus.',
+        9,
+        7
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Toy Keyboard',
+        275.05,
+        17,
+        'Nam dui.',
+        7,
+        2
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Decorative Bowls',
+        496.55,
+        22,
+        'Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.',
+        10,
+        6
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Harry Potter and the Sorcerer Stone',
+        961.3,
+        37,
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+        7,
+        5
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Soccer Ball',
+        269.0,
+        100,
+        'Suspendisse accumsan tortor quis turpis. Sed ante.',
+        8,
+        10
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Mirrors',
+        494.68,
+        31,
+        'Nulla tellus.',
+        8,
+        6
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Smart Plug',
+        693.73,
+        247,
+        'Duis bibendum. Morbi non quam nec dui luctus rutrum.',
+        10,
+        9
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Bike Lock',
+        260.98,
+        426,
+        'Integer a nibh.',
+        2,
+        10
+    );
+
+insert into
+    products (
+        product_name,
+        price,
+        inventory,
+        description,
+        seller_id,
+        category_id
+    )
+values (
+        'Hair Serum ',
+        917.46,
+        47,
+        'Nullam porttitor lacus at turpis.',
+        5,
+        1
+    );
---
+++ Task 5 - Sql/sainikhil/run query.sql
@@ -0,0 +1,54 @@
+-- details of all the sellers who are selling home and decor products.
+
+SELECT
+    seller_name,
+    category_name
+FROM sellers
+    LEFT JOIN products ON sellers.seller_id = products.seller_id
+    LEFT JOIN categories ON products.category_id = categories.category_id
+WHERE
+    categories.category_name = 'Home Decor';
+
+    
+
+-- details of users who have address in hyderabad.
+
+SELECT * FROM customer_address WHERE city = 'Hyderabad';
+
+
+
+-- all the products list which have more then 100 items in inventory
+
+SELECT * FROM products WHERE inventory >100;
+
+
+
+-- all the users list who have orders greater than 10 with in one week
+
+SELECT
+    c.first_name || '' || c.last_name AS customer_name
+FROM customers c
+    INNER JOIN orders o ON c.customer_id = o.customer_id
+WHERE
+    CURRENT_DATE - o.order_date <= 7
+GROUP BY c.customer_id
+HAVING COUNT(o.order_id) > 10;
+
+
+
+-- all the orders which have delivery time more than 5 days
+
+SELECT * FROM orders WHERE promised_delivery_date - order_date > 5;
+
+
+
+-- all the details of users who have purchased products using same credit card more than 5 times.
+
+SELECT
+    first_name || '' || last_name as customer_name
+FROM customers c
+    INNER JOIN orders o ON c.customer_id = o.customer_id
+    INNER JOIN cards ca on o.card_id = ca.card_id
+WHERE ca.card_type = 'Credit'
+GROUP BY c.customer_id
+HAVING COUNT(ca.card_id) > 5;

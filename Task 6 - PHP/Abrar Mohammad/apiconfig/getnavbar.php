7e99db79a6045895dbcffd7851c1ab0296f783fa
abrar525 <abrarmohammad795@gmail.com>
2023-08-26 13:46:47 +0530

    task_6 completed

---
+++ Task 6 - PHP/Abrar Mohammad/apiconfig/getnavbar.php
@@ -0,0 +1,38 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", "1");
+
+include 'databaseconfig.php';
+
+$response = ["status" => false, "message" => "", "user_name" => "", "data" => []];
+
+try {
+    $query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';";
+    $stmt = $pdo->query($query);
+    $tableNames = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+    $response["status"] = true;
+    $response["message"] = "Successful!";
+    $response["data"] = $tableNames;
+
+    if (isset($_GET['userToken'])) {
+        $token = $_GET['userToken'];
+        $userQuery = "SELECT firstname,lastname FROM register WHERE token = ?";
+        $stmt = $pdo->prepare($userQuery);
+        $stmt->execute([$token]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            $response["user_name"] = $result['firstname'] . ' ' . $result["lastname"];
+        } else {
+            $response["status"] = false;
+            $response["message"] = "User not found.";
+        }
+    }
+
+} catch (PDOException $e) {
+    $response["message"] = $e->getMessage();
+}
+
+echo json_encode($response);
+?>
---
+++ Task 6 - PHP/Abrar Mohammad/apiconfig/register.php
@@ -0,0 +1,59 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+include 'databaseconfig.php';
+$response = ["status" => false, "message" => ""];
+
+if (!isset($_POST["firstName"])) {
+    $response["status"] = false;
+    $response["message"] = "First name field is not included";
+    echo json_encode($response);
+    exit;
+}
+
+if (!isset($_POST["lastName"])) {
+    $response["status"] = false;
+    $response["message"] = "Last name Field is not included";
+    echo json_encode($response);
+    exit;
+}
+
+if (!isset($_POST["email"])) {
+    $response["status"] = false;
+    $response["message"] = "Email Field is not included";
+    echo json_encode($response);
+    exit;
+}
+
+if (!isset($_POST["password"])) {
+    $response["status"] = false;
+    $response["message"] = "Password field is not included";
+    echo json_encode($response);
+    exit;
+}
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password = md5($password);
+$query = "SELECT id FROM register WHERE email = '$email' AND password = '$password'";
+$statement = $pdo->query($query);
+$res = $statement->fetch();
+if ($res && $res['id']) {
+    $response["status"] = false;
+    $response["message"] = $email . " already exists";
+    echo json_encode($response);
+    exit;
+}
+$insertQuery = "INSERT INTO register(firstname,lastname,email,password) VALUES(?,?,?,?);";
+
+$stmt = $pdo->prepare($insertQuery);
+$insertRes = $stmt->execute([$firstName, $lastName, $email, $password]);
+if ($insertRes) {
+    $response["status"] = true;
+    $response["message"] = "User Registered Successfully";
+    echo json_encode($response);
+    exit;
+}
+?>
